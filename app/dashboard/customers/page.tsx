9c964eae024ccb92feb317f964d7647a7286a086
alex ochoa <aochoa@copeco.gob.hn>
2024-04-15 17:17:15 -0600

    cambios

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+
+export default function Page(){
+    return <p>Custumer Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Creepster } from 'next/font/google';
+
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight:'400', subsets:['latin']});
+export const creepster = Creepster({weight:'400', subsets:['latin']})
